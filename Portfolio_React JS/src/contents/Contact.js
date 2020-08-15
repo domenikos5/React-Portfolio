bb2237ae5b9f0437ab4477e1ff639221e4d4fbfa
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-08-15 06:04:20 -0700

    polishing

--- Portfolio_React JS/src/contents/Contact.js
+++ Portfolio_React JS/src/contents/Contact.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import Social from '../components/Social';
+import React, { Component } from "react";
+import Social from "../components/Social";
 class Contact extends Component {
-    render() {
-        return (
-            <div className="condiv">
-            <h1 className="subtopic">Contact Me</h1>
-            <h3>Email  :   domenikos858@gmail.com</h3>
-            
-            <Social /> 
-            </div>
-            )
-        }
-    }
-    
-    export default Contact
-    
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">Contact Me</h1>
+        <h3>Email : domenikos858@gmail.com</h3>
+        <Social />
+      </div>
+    );
+  }
+}
+
+export default Contact;
